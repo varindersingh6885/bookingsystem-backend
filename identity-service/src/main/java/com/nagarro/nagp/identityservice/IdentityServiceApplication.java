26e6621d0b2169ff51f96844e81af6306219e507
Varinder Singh <varinder.singh01@nagarro.com>
2023-03-27 14:56:21 +0530

    identity service init

---
+++ identity-service/src/main/java/com/nagarro/nagp/identityservice/IdentityServiceApplication.java
@@ -0,0 +1,15 @@
+package com.nagarro.nagp.identityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class IdentityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityServiceApplication.class, args);
+	}
+
+}
